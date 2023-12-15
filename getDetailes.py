6aa99fcdba9c9beaf8a7d5220abbdd66ed432680
Parsa Bordbar <parsab71@gmail.com>
2023-12-15 13:31:10 +0330

    get_books now can return a Lsit of Dictinoaries with Books details. parse_to_json now can make a single json file, note that it is still in devlopment and not finalized

--- getDetailes.py
+++ getDetailes.py
@@ -4,26 +4,16 @@ import json
 
 #Note: The get_books Func Will be updated to generate Dicts for each book. P.B.
 
-#A Python Dictionary for a single books Details
-book_details = {
-    'title': ...,
-    'author': ...,
-    'imgLink': ...,
-    'desc': ...,
-    'Categories': ...,
-    'Year': ...,
-    'Edition': ...,
-    'Publisher': ...,
-    'Language': ...,
-    'Pages': ...,
-    'ISBN 10': ...,
-    'ISBN 13': ...,
-    'Series': ...,
-    'File': ...,
-    'IPFS CID': ...,
-    'IPFS CID blake2b': ...,
-}
+import json
 
+class JSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if obj is Ellipsis:
+            return 'detail_dict'
+        return super().default(obj)
+
+
+books_detail_list = []
 
 main_url = r'https://singlelogin.re'
 page_categories = '/categories'
@@ -44,7 +34,6 @@ def get_books (categories, category_num):
     url_category = categories[category_num]['href']
     index_of_cover_image = 1
 
-    # print(url_category)
     response = requests.get(main_url + url_category)
     soup = BeautifulSoup(response.text, 'html.parser')
     
@@ -55,95 +44,111 @@ def get_books (categories, category_num):
         print(book.text)
         url_book = book['href']
 
-        # print(url_book)
         response = requests.get(main_url + url_book)
         
         #Getting Tittle of Book
         soup = BeautifulSoup(response.text, 'html.parser')
         book_title = soup.select('h1')[0].text.strip()
-        print('Book Title: ' + book_title)
 
         #Getting Books Author
         soup = BeautifulSoup(response.text, 'html.parser')
         book_author = soup.select('.col-sm-9 .color1')[0].text.strip()
-        print('Book author: ' + book_author)
 
         #Getting Books Description
         soup = BeautifulSoup(response.text, 'html.parser')
         book_desc = soup.select('#bookDescriptionBox')[0].text.strip()
-        print('Book desc: ' + book_desc)
 
         #Getting Book Category
         book_cat = soup.select('.property_value a')[0].text
-        print('Book Cate: ' + book_cat)
 
         #Getting Book Publisher (if There is one!)
         try:
             book_publisher = soup.select('.property_publisher .property_value')[0].text
-            print('Book Publisher: ' + book_publisher)
         except:
-            print('No publisher!')
+            book_publisher = 'No publisher!'
 
         #Getting Book url for It's Picture
         soup = BeautifulSoup(response.text, 'html.parser')
         book_img = soup.select('img')[index_of_cover_image]['src']
-        print('Book img: ' + book_img)
         
-                #Getting Book Edition
+        #Getting Book Edition
         try:
-          book_edition = soup.select('.property_edition .property_value')[0].text
-          print( 'Book Edition: ' + book_edition)
+            book_edition = soup.select('.property_edition .property_value')[0].text
         except:
-            print('No Edition!')
+            book_edition = 'No Edition!'
 
         #Getting Book IPFS CID blake2b
-        book_cid_blake = soup.select('.property_ipfs_blake2b_cid span ')[0].text
-        print( 'Book IPFS CID blacke2b : ' + book_cid_blake)
+        try:
+            book_cid_blake = soup.select('.property_ipfs_blake2b_cid span ')[0].text
+        except:
+            book_cid_blake = 'No Book IPFS CID!'
         
         #Getting Book IPFS CID 
-        book_cid = soup.select('.property_ipfs_cid span')[0].text
-        print( 'Book IPFS CID : ' + book_cid)
+        try:
+            book_cid = soup.select('.property_ipfs_cid span')[0].text
+        except:
+            book_cid = ' No Book IPFS CID!'
 
         #Getting Book ISBN 10 
         try:
             book_isbn_10 = soup.select(' .property_value .{10}')[0].text
-            print( 'Book ISBN 10  : ' + book_isbn_10)
         except:
-            print('No box ISBN10')
+            book_isbn_10 = 'No box ISBN10'
         
         #Getting Book Year
         book_year = soup.select('.property_year .property_value')[0].text
-        print('Book Year : ' + book_year)
 
         #Getting book pages
         book_pages = soup.select('.property_pages .property_value')[0].text
-        print('book pages : ' + book_pages)
 
         #Getting book ISBN 13
         try:
             book_isbn_13 = soup.select('.13 .property_value')[0].text
-            print('book ISBN 13 : ' + book_isbn_13)
         except:
-            print('No ISBN13')
+            book_isbn_13 = 'No ISBN13'
+        
         #Getting book Series
         try:
           book_series = soup.select('.property_series .property_value')[0].text
-          print('book series : ' + book_series)
         except:
-          print('No Series!')
+          book_series = 'No Series!'
 
         #Getting book file
         book_file = soup.select('.property__file .property_value')[0].text
-        print('book file : ' + book_file)
-        print('_______________________________________________________________')
-
+        
+        #A Python Dictionary for a single books Details
+        detail_dict = dict(
+            title= book_title,
+            imgLink= book_img,
+            author= book_author,
+            desc= book_desc,
+            Categories= book_cat,
+            Year= book_year,
+            Edition= book_edition,
+            Publisher= book_publisher,
+            Language= ...,
+            Pages= book_pages,
+            ISBN_10= book_isbn_10,
+            ISBN_13= book_isbn_13,
+            Series= book_series,
+            File= book_file,
+            IPFS_CID= book_cid,
+            IPFS_CID_blake2b= book_cid_blake,
+                           )
+        books_detail_list.append(detail_dict)
+        return books_detail_list
 
 
 #The Dictionary From get_books will be parsed into To Json here!
-def parse_to_json(detail_dic):
-    json_object = json.dumps(detail_dic, indent=16)
-    with open("details.json", "w") as outfile:
-        outfile.write(json_object)
+def parse_to_json(books_detail_list):
+    for detail_dict in books_detail_list:
+            filename = detail_dict['title'].replace(' ', '_')
+            data = {filename : detail_dict}
+            json_object = json.dumps(data, cls=JSONEncoder)
+            with open(filename, "w") as outfile:
+                outfile.write(json_object)
+            continue
 
 catagories = get_categories(main_url, page_categories)
-get_books(catagories, 0)
+details = get_books(catagories, 0)
+parse_to_json(details)
