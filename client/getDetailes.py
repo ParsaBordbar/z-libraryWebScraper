0835d86af8a6ba01f64fca70e914bd9a514cee03
Parsa Bordbar <parsab71@gmail.com>
2023-12-27 13:38:43 +0330

    db's collections are now validated, dictionaty fildes updated.

--- client/getDetailes.py
+++ client/getDetailes.py
@@ -108,8 +108,11 @@ def get_books (category):
         book_title = soup.select('h1')[0].text.strip()
 
         #Getting Books Author
-        soup = BeautifulSoup(response.text, 'html.parser')
-        book_author = soup.select('.col-sm-9 .color1')[0].text.strip()
+        try:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            book_author = soup.select('.col-sm-9 .color1')[0].text.strip()
+        except:
+            book_author = None
 
         #Getting Books Description
         try:
@@ -124,7 +127,7 @@ def get_books (category):
         try:
             book_publisher = soup.select('.property_publisher .property_value')[0].text
         except:
-            book_publisher = 'No publisher!'
+            book_publisher = None
 
         #Getting Book url for It's Picture
         soup = BeautifulSoup(response.text, 'html.parser')
@@ -140,19 +143,19 @@ def get_books (category):
         try:
             book_cid_blake = soup.select('.property_ipfs_blake2b_cid span ')[0].text
         except:
-            book_cid_blake = 'No Book IPFS CID!'
+            book_cid_blake = None
         
         #Getting Book IPFS CID 
         try:
             book_cid = soup.select('.property_ipfs_cid span')[0].text
         except:
-            book_cid = ' No Book IPFS CID!'
+            book_cid = None
 
         #Getting Book ISBN 10 
         try:
             book_isbn_10 = soup.select(' .property_value .{10}')[0].text
         except:
-            book_isbn_10 = 'No box ISBN10'
+            book_isbn_10 = None
         
         #Getting Book Year
         book_year = soup.select('.property_year .property_value')[0].text
@@ -161,13 +164,13 @@ def get_books (category):
         try:
             book_pages = soup.select('.property_pages .property_value')[0].text
         except:
-            book_pages = "No data"
+            book_pages = None
         
         #Getting book ISBN 13
         try:
             book_isbn_13 = soup.select('.13 .property_value')[0].text
         except:
-            book_isbn_13 = 'No ISBN13'
+            book_isbn_13 = None
         
         #Getting book Series
         try:
@@ -178,25 +181,33 @@ def get_books (category):
         #Getting book file
         book_file = soup.select('.property__file .property_value')[0].text
         
+        #Getting the Book's Language
+        try:
+            book_language = soup.select('.text-capitalize')[0].text
+        except:
+            book_language = None
+        
+                
         #A Python Dictionary for a single books Details
         detail_dict = dict(
             title = book_title,
             imgLink= book_img,
             author= book_author,
             desc= book_desc,
-            Categories= book_cat,
-            Year= book_year,
-            Edition= book_edition,
-            Publisher= book_publisher,
-            Language= ...,
-            Pages= book_pages,
-            ISBN_10= book_isbn_10,
-            ISBN_13= book_isbn_13,
-            Series= book_series,
-            File= book_file,
-            IPFS_CID= book_cid,
-            IPFS_CID_blake2b= book_cid_blake,
-                           )
+            **({'Categories': book_cat} if book_cat else {}),
+            **({'Year': book_year} if book_year else {}),
+            **({'Edition': book_edition} if book_edition else {}),
+            **({'Publisher': book_publisher} if book_publisher else {}),
+            **({'Language': book_language} if book_language else {}),
+            **({'Pages': book_pages} if book_pages else {}),
+            **({'ISBN_10': book_isbn_10} if book_isbn_10 else {}),
+            **({'ISBN_13': book_isbn_13} if book_isbn_13 else {}),
+            **({'File': book_file} if book_file else {}),
+            **({'IPFS_CID': book_cid} if book_cid else {}),
+            **({'IPFS_CID_blake2b': book_cid_blake} if book_cid_blake else {}),
+            **({'Series': book_series} if book_series else {}),
+        )
+
         books_detail_list.append(detail_dict)
     return books_detail_list
 
@@ -223,20 +234,15 @@ def parse_to_json(detail_dict, category_dir):
     filename = detail_dict['title'] + '.json'
     titleBook = detail_dict['title'] 
     split = category_dir.split('\\')
-    print('khvhkvopgpog ',split)
     final_res = split[len(split) - 1]
     print(split[len(split) - 1])
-    print("{}\{}.json".format(category_dir, final_res))
+    print("{category_dir}\{final_res}.json")
     valid_file_name = file_name_validation(filename)
     data =  detail_dict
-    print('ouoewguwegweg  ' + "{}\{}.json".format(category_dir, final_res))
     json_object = json.dumps(data, cls=JSONEncoder)
 
     with open(category_dir + "\\" + valid_file_name, "w") as outfile:
         outfile.write(json_object)
-    # print(json_data)
-    # print((detail_dict))
-    # print(type(json_object))
     load_file(category_dir , final_res)
     new_user = json_object.title
     print(new_user)
