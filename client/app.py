afaa6bab66ced31571462856523f65cc6011d4b2
Sadegh Ghasemi <95068600+Sadesm@users.noreply.github.com>
2024-01-20 14:13:59 +0330

    Update app.py

--- client/app.py
+++ client/app.py
@@ -27,28 +27,6 @@ def selfCato(menu):
 @app.route('/search')
 def search():
     return render_template('search.html')
-
-# Now you can use the 'mongo' object to interact with your MongoDB database
-@app.route('/api/insert', methods=['POST'])
-def insert_document(new_book):
-    users = mongo.db.books
-    result = users.count_documents(new_book)
-    # Check if the result exists
-    if result > 0:
-        print("The JSON file is already in the database.")
-    else:
-        print("The JSON file is not in the database.")
-        result = users.insert_one(new_book)
-        return str(result)
-
-
-@app.route('/api/find', methods=['GET'])
-def get_all_data():
-        collection = mongo.db.books
-        data = list(collection.find({}))  # Retrieve all documents from the collection
-        # Convert ObjectId to string using json_util
-        json_data = json_util.dumps(data)
-        return json_data, 200
     
 if __name__ == '__main__':
     app.run(debug = True)
